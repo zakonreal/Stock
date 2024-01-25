e01b8fbf96c0302334f1d8a49e5710809ab25303
unknown <106015877+zakonreal@users.noreply.github.com>
2024-01-25 14:05:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -484,41 +484,40 @@ for message in st.session_state.messages:
             st.markdown(message.content)
 
 if prompt := st.chat_input():
-        if not access_token and not credentials and not (user and password):
-            st.info("Заполните данные GigaChat для того, чтобы продолжить")
-            st.stop()
-    
-        chat = GigaChat(
-            base_url=base_url,
-            credentials=credentials,
-            access_token=st.session_state.get("token")
-            or access_token,  # Переиспользуем токен
-            user=user,
-            password=password,
-            verify_ssl_certs=False,
-        )
-    
-        message = ChatMessage(role="user", content=prompt)
-        st.session_state.messages.append(message)
-    
-        with st.chat_message(message.role):
-            st.markdown(message.content)
-    
-        message = ChatMessage(role="assistant", content="")
-        st.session_state.messages.append(message)
-    
-        with st.chat_message(message.role):
-            message_placeholder = st.empty()
-            for chunk in chat.stream(st.session_state.messages):
-                message.content += chunk.content
-                message_placeholder.markdown(message.content + "▌")
-            message_placeholder.markdown(message.content)
-    
-        # Каждый раз, когда пользователь нажимает что-то в интерфейсе весь скрипт выполняется заново.
-        # Сохраняем токен и закрываем соединения
-        st.session_state.token = chat._client.token
-        chat._client.close()
+    if not access_token and not credentials and not (user and password):
+        st.info("Заполните данные GigaChat для того, чтобы продолжить")
+        st.stop()
 
+    chat = GigaChat(
+        base_url=base_url,
+        credentials=credentials,
+        access_token=st.session_state.get("token")
+        or access_token,  # Переиспользуем токен
+        user=user,
+        password=password,
+        verify_ssl_certs=False,
+    )
+
+    message = ChatMessage(role="user", content=prompt)
+    st.session_state.messages.append(message)
+
+    with st.chat_message(message.role):
+        st.markdown(message.content)
+
+    message = ChatMessage(role="assistant", content="")
+    st.session_state.messages.append(message)
+
+    with st.chat_message(message.role):
+        message_placeholder = st.empty()
+        for chunk in chat.stream(st.session_state.messages):
+            message.content += chunk.content
+            message_placeholder.markdown(message.content + "▌")
+        message_placeholder.markdown(message.content)
+
+    # Каждый раз, когда пользователь нажимает что-то в интерфейсе весь скрипт выполняется заново.
+    # Сохраняем токен и закрываем соединения
+    st.session_state.token = chat._client.token
+    chat._client.close()
 
 # In[3]:
 
